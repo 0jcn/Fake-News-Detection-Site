d09b3e5a95f286273d077ea2e1dad18b89f34651
Jacob Neubauer <0jcn@pm.me>
2026-03-22 21:28:12 +0000

    Removing context and subject from input as not needed

--- MiddleLayerAPI/Models/ModelInput.cs
+++ MiddleLayerAPI/Models/ModelInput.cs
@@ -6,13 +6,6 @@
         /// News Statement
         /// </summary>
         public string? statement { get; set; }   
-        /// <summary>
-        /// News Subject
-        /// </summary>
-        public string? subject { get; set; }
-        /// <summary>
-        /// News Context
-        /// </summary>
-        public string? context { get; set; }
+        
     }
 }
