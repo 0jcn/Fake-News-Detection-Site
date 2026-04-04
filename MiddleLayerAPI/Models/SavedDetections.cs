654de41849607753fe7664ed02f2ea225bd0ada2
Jacob Neubauer <0jcn@pm.me>
2026-04-04 13:02:06 +0100

    WIP - Adding docker file, table definitions for postgres db. Also adding in base model into ASP.net API for handling these

---
+++ MiddleLayerAPI/Models/SavedDetections.cs
@@ -0,0 +1,25 @@
+﻿namespace MiddleLayerAPI.Models
+{
+    public class SavedDetections
+    {
+        public int DetectionId { get; set; }    
+
+        public int UserId { get; set; } 
+
+        public string Input { get; set; } = string.Empty;
+
+        public float? Result { get; set; }
+        
+        public float? TrueProbability { get; set; }
+
+        public float? BarelyTrueProbability { get; set; }
+
+        public float? HalfTrueProbability { get; set; } 
+
+        public float? FalseProbability { get; set; }
+
+        public float? MostlyTrueProbability { get; set; }   
+
+        public float? PantsFireProbability { get; set; }    
+    }
+}
