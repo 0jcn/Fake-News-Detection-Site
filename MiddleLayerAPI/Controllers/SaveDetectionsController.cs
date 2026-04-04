a64535281f076c73b4d416d71d596e69056eb8b6
Jacob Neubauer <0jcn@pm.me>
2026-04-04 14:57:47 +0100

    WIP - Adding code to controllers for handling db requests

--- MiddleLayerAPI/Controllers/SaveDetectionsController.cs
+++ MiddleLayerAPI/Controllers/SaveDetectionsController.cs
@@ -4,12 +4,34 @@ using MiddleLayerAPI.Models;
 
 namespace MiddleLayerAPI.Controllers
 {
+    [ApiController]
+    [Route("Detections")]
     public class SaveDetectionsController : Controller
     {
         private readonly AppSettings _appSettings;
         public SaveDetectionsController(IOptions<AppSettings> appSettings)
         {
             _appSettings = appSettings.Value;
+        }
+        [HttpPost]
+        public IActionResult SaveDetection([FromBody] SavedDetections detection)
+        {
+            var response = _appSettings.DbHelper.SaveDetection(detection).Result;
+            if (response)
+            {
+                return new JsonResult(Ok("Detection saved successfully"));
+            }
+            return new JsonResult(NoContent());
+        }
+        [HttpDelete("/{detectionId}")]
+        public IActionResult RemoveSavedDetection(int detectionId)
+        {
+            var response = _appSettings.DbHelper.RemoveSavedDetection(detectionId).Result;
+            if (response)
+            {
+                return new JsonResult(Ok("Detection removed successfully"));
+            }
+            return new JsonResult(NoContent());
         }
     }
 }
--- MiddleLayerAPI/Controllers/UserController.cs
+++ MiddleLayerAPI/Controllers/UserController.cs
@@ -4,6 +4,8 @@ using MiddleLayerAPI.Models;
 
 namespace MiddleLayerAPI.Controllers
 {
+    [ApiController]
+    [Route("Users")]
     public class UserController : Controller
     {
         private readonly AppSettings _appSettings;
@@ -12,8 +14,47 @@ namespace MiddleLayerAPI.Controllers
         {
             _appSettings = appSettings.Value;
         }
+        [HttpPost]
+        public IActionResult CreateUser([FromBody] Users newUser)
+        {
+            var response = _appSettings.DbHelper.CreateUser(newUser).Result;
+            if (response != null)
+            {
+                return new JsonResult(Ok(response));
+            }
+            return new JsonResult(NoContent());
 
+        }
+        [HttpGet("/{userId}")]
+        public IActionResult GetUser(int userId)
+        {
+            var response = _appSettings.DbHelper.GetUser(userId).Result;
+            if (response != null)
+            {
+                return new JsonResult(Ok(response));
+            }
+            return new JsonResult(NoContent());
 
-
+        }
+        [HttpPut]
+        public IActionResult updateUser([FromBody] Users updatedUser)
+        {
+            var response = _appSettings.DbHelper.UpdateUser(updatedUser).Result;
+            if (response != null)
+            {
+                return new JsonResult(Ok(response));
+            }
+            return new JsonResult(NoContent());
+        }
+        [HttpDelete("/{userId}")]
+        public IActionResult deleteUser(int userId)
+        {
+            var response = _appSettings.DbHelper.DeleteUser(userId).Result;
+            if (response)
+            {
+                return new JsonResult(Ok("User deleted successfully"));
+            }
+            return new JsonResult(NoContent());
+        }
     }
 }
